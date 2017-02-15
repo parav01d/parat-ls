0bc23c6f370b5fe892da5c19ef35521f87a77752
Kunzemann <nextbike.kunzemann@gmail.com>
2017-02-15 20:47:37 +0100

    add cat script ... meow meow

---
+++ cat/cat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+cat_header='    ___________________________\n  /\n |'
+cat_footer=' |\n  \\____    _________________________________________\n       \\  /\n        \\/ \n'
+
+cat_sounds=()
+cat_sounds[0]='cat1.wav'
+cat_sounds[1]='cat2.wav'
+
+cats=()
+cats[0]='     /\\__/\\\n    /`    `\\ \n  === 0  0 ===\n    \\  --  /\n   /        \\\n  /          \\\n |            |\n  \\  ||  ||  /\n   \\_oo__oo_/#######o'
+cats[1]='   /\\     /\\\n  {  `---`  }\n  {  O   O  }\n~~|~   V   ~|~~\n   \  \|/  /\n    `-----`__\n    /     \  `^\_\n   {       }\ |\_\_   W\n   | \_\/  |/ /  \\_\\_( )\n    \\__/  /(_E     \\__/\n      (  /\n       MM'
+
+echo "${bold}${cat_header}"
+while IFS='' read -r line || [[ -n "$line" ]]; do
+echo "${bold} |  ${normal}$line"
+done < "$1"
+echo "${bold}${cat_footer}"
+echo "${bold}${cats[$RANDOM % 2]}"
+echo "${normal}"
+eval "afplay ${PWD}/${cat_sounds[$RANDOM % 2]}"
+
